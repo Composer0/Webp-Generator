cc6f2e17234373e741903e1dde1d244442bab88c
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2023-08-28 14:20:28 -0400

    update

--- bs-config.js
+++ bs-config.js
@@ -1,16 +1,15 @@
 // bs-config.js
 
 module.exports = {
-    server: {
-      middleware: {
-        // Add cross-origin headers
-        1: function(req, res, next) {
-          res.setHeader('Access-Control-Allow-Origin', '*');
-          res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-          res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-          next();
-        }
+  server: {
+    middleware: {
+      // Add cross-origin headers
+      1: function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        next();
       }
     }
-  };
-  
+  }
+};
